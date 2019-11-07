8de6e02dadd0df08162a63eccfd4678aaf243611
xiaoke <15072770488@163.com>
2019-11-07 23:36:35 +0800

    新建工程

---
+++ parent/health_common/src/main/java/com/xiaoke/pojo/Student.java
@@ -0,0 +1,14 @@
+package com.xiaoke.pojo;
+
+/**
+ * @program: day1107
+ * @description:
+ * @author: xiaoke
+ * @create: 2019-11-07 23:04
+ **/
+
+public class Student {
+    String username;
+    int age;
+
+}
